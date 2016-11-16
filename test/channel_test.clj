2cd82324ae58513e1a25275a7b4e375b8494cf12
Janwillem Swalens <jswalens@vub.ac.be>
2016-11-16 16:59:39 +0100

    Add two more tests.

--- test/channel_test.clj
+++ test/channel_test.clj
@@ -30,3 +30,29 @@
         (channel/put c i))
       (dotimes [i 100]
         (is (= i (channel/take c)))))))
+
+(deftest hundred-receivers
+  (testing "1 sender, 100 receivers"
+    (let [c (channel/channel)
+          sender
+            (future
+              (dotimes [i 100]
+                (channel/put c i)))
+          receivers
+            (doall (for [i (range 100)]
+              (future
+                (channel/take c))))]
+      (is (= 4950 (reduce + (map deref receivers)))))))
+
+(deftest hundred-senders
+  (testing "100 senders, 1 receiver"
+    (let [c (channel/channel)
+          senders
+            (dotimes [i 100]
+              (future
+                (channel/put c i)))
+          receiver
+            (future
+              (doall (for [i (range 100)]
+                (channel/take c))))]
+      (is (= 4950 (reduce + @receiver))))))
